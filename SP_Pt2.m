2ff13db4d9b19100c02d8bc20700693c6726943b
Iván Delgado Ramos <93808733+Ivancitop@users.noreply.github.com>
2025-06-11 22:33:59 -0600

    Update SP_Pt2.m

--- SP_Pt2.m
+++ SP_Pt2.m
@@ -1,5 +1,5 @@
-%Simulación de sistema 1/4 suspensión
-%Definición de sistema y parámetros:
+Simulación de sistema 1/4 suspensión
+Definición de sistema y parámetros:
 % Parámetros
 m1 = 560;     % masa del carro (kg)
 m2 = 45;      % masa de la llanta (kg)
@@ -19,60 +19,114 @@ B = [0;
      0;
      k2/m2 + b2/m2];
 
-C = [1 0 0 0];  % salida: desplazamiento carrocería
-D = 0;
-
-% Definir sistema
-sys = ss(A, B, C, D);
+C_full = eye(4);  % observamos todos los estados
+D_full = zeros(4,1);
+sys_full = ss(A, B, C_full, D_full);
 
 % Tiempo
 t = 0:0.01:5;  % 5 segundos
 
 Entrada de escalón
 u1 = ones(size(t));  % escalón unitario
-[y1, ~] = lsim(sys, u1, t);
+% Simulación completa para escalón
+[y_step, ~] = lsim(sys_full, u1, t);  % y_step contiene x1, x2, x3, x4
+
+% Aceleración del carro: derivada de la velocidad (x2)
+a_step = [0; diff(y_step(:,2))./diff(t')];  % estimación numérica
 
 figure;
-plot(t, y1, 'LineWidth', 2);
+subplot(3,1,1);
+plot(t, y_step(:,1), 'LineWidth', 2);
+ylabel('Desplazamiento (m)');
 title('Respuesta a entrada tipo Escalón');
-xlabel('Tiempo (s)');
-ylabel('Desplazamiento x_1 (m)');
 grid on;
+
+subplot(3,1,2);
+plot(t, y_step(:,2), 'LineWidth', 2);
+ylabel('Velocidad (m/s)');
+grid on;
+
+subplot(3,1,3);
+plot(t, a_step, 'LineWidth', 2);
+ylabel('Aceleración (m/s²)');
+xlabel('Tiempo (s)');
+grid on;
+
 
 Entrada de rampa
-u2 = t;  % entrada rampa
-[y2, ~] = lsim(sys, u2, t);
+u2 = t;
+[y_ramp, ~] = lsim(sys_full, u2, t);
+a_ramp = [0; diff(y_ramp(:,2))./diff(t')];
 
 figure;
-plot(t, y2, 'LineWidth', 2);
+subplot(3,1,1);
+plot(t, y_ramp(:,1), 'LineWidth', 2);
 title('Respuesta a entrada tipo Rampa');
-xlabel('Tiempo (s)');
-ylabel('Desplazamiento x_1 (m)');
+ylabel('Desplazamiento (m)');
 grid on;
+
+subplot(3,1,2);
+plot(t, y_ramp(:,2), 'LineWidth', 2);
+ylabel('Velocidad (m/s)');
+grid on;
+
+subplot(3,1,3);
+plot(t, a_ramp, 'LineWidth', 2);
+ylabel('Aceleración (m/s²)');
+xlabel('Tiempo (s)');
+grid on;
+
 
 Entrada de perfil senoidal (camino ondulado)
-f = 1;  % frecuencia 1 Hz
-u3 = sin(2*pi*f*t);  % entrada senoidal
-[y3, ~] = lsim(sys, u3, t);
+% Entrada senoidal (1 Hz)
+f = 1;
+u3 = sin(2*pi*f*t);
+[y_seno, ~] = lsim(sys_full, u3, t);
+a_seno = [0; diff(y_seno(:,2))./diff(t')];
 
 figure;
-plot(t, y3, 'LineWidth', 2);
-title('Respuesta a entrada tipo Senoide (camino ondulado)');
-xlabel('Tiempo (s)');
-ylabel('Desplazamiento x_1 (m)');
+subplot(3,1,1);
+plot(t, y_seno(:,1), 'LineWidth', 2);
+title('Respuesta a entrada tipo Senoidal');
+ylabel('Desplazamiento (m)');
 grid on;
 
+subplot(3,1,2);
+plot(t, y_seno(:,2), 'LineWidth', 2);
+ylabel('Velocidad (m/s)');
+grid on;
+
+subplot(3,1,3);
+plot(t, a_seno, 'LineWidth', 2);
+ylabel('Aceleración (m/s²)');
+xlabel('Tiempo (s)');
+grid on;
+
+
 Entrada de Road Bump (bache localizado)
+% Entrada tipo bache
 u4 = zeros(size(t));
 bump_duration = (t >= 1 & t <= 2);
 u4(bump_duration) = sin(pi*(t(bump_duration) - 1));  % medio senoide
 
-[y4, ~] = lsim(sys, u4, t);
+[y_bump, ~] = lsim(sys_full, u4, t);
+a_bump = [0; diff(y_bump(:,2))./diff(t')];
 
 figure;
-plot(t, y4, 'LineWidth', 2);
+subplot(3,1,1);
+plot(t, y_bump(:,1), 'LineWidth', 2);
 title('Respuesta a entrada tipo Bache (Road bump)');
+ylabel('Desplazamiento (m)');
+grid on;
+
+subplot(3,1,2);
+plot(t, y_bump(:,2), 'LineWidth', 2);
+ylabel('Velocidad (m/s)');
+grid on;
+
+subplot(3,1,3);
+plot(t, a_bump, 'LineWidth', 2);
+ylabel('Aceleración (m/s²)');
 xlabel('Tiempo (s)');
-ylabel('Desplazamiento x_1 (m)');
 grid on;
 
